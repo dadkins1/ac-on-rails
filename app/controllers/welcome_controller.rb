9f74f9f0b28e25cb832015e7a45f1f28f007ecba
dadkins <amedeo602@hotmail.com>
2019-11-30 19:56:22 -0500

    Added fuzzy search feature

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @devices = Device.all
-    @new_device = Device.new
+    if params[:search].blank?
+      @devices = Device.all
+    else
+      @search_sn = params[:search].downcase
+      @devices = Device.all.where("lower(serial_number) LIKE :search", search: "%#{@search_sn}%")
+    end
   end
 end
